f5fc590bb245e210723c577cc3a8924cc78673a1
DELL 5290 <DELL 5290@DESKTOP-7S1MOM9>
2022-11-22 15:58:35 +0530

    Add project files.

---
+++ Demo/Models/DemoContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Demo.Models
+{
+    public partial class DemoContext : DbContext
+    {
+        public DemoContext()
+        {
+        }
+
+        public DemoContext(DbContextOptions<DemoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblUser> TblUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-7S1MOM9;Database=Demo;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblUser>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("tblUsers");
+
+                entity.Property(e => e.Contactno)
+                    .HasMaxLength(50)
+                    .HasColumnName("contactno");
+
+                entity.Property(e => e.Dob)
+                    .HasMaxLength(50)
+                    .HasColumnName("dob");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Firstname)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnName("firstname");
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(50)
+                    .HasColumnName("gender");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Lastname)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnName("lastname");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Role)
+                    .HasMaxLength(50)
+                    .HasColumnName("role");
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Demo/Models/TblUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Demo.Models
+{
+    public partial class TblUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Dob { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Contactno { get; set; }
+        public string Gender { get; set; }
+    }
+}
